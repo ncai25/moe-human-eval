2627c54e292305cdfdbc5c68753a6f115c886096
norafk <noracai0625@gmail.com>
2026-03-29 12:38:11 -0400

    new explaination

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { ExpertListView } from "./components/ExpertListView";
 import { AnnotationView } from "./components/AnnotationView";
 import { Download } from "lucide-react";
 import { Button } from "./components/ui/button";
-import expertSample from "../imports/expert-manual.json";
+import expertSample from "../imports/expert-manual2.json";
 import React from "react";
 
 interface Example {
